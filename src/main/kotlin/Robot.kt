a5a233ceeb9058890e323b392f8dffc055fb93c5
David Bauducco <antirobo1@gmail.com>
2018-04-28 23:11:04 -0500

    First Commit
    Signed-off-by: David Bauducco <david@lifepg.com>

---
+++ src/main/kotlin/Robot.kt
@@ -0,0 +1,11 @@
+package frc.team3735.robot
+
+import edu.wpi.first.wpilibj.IterativeRobot
+
+class Robot : IterativeRobot() {
+    override fun robotInit() {
+        println("Hello World from Kotlin!")
+    }
+
+
+}
